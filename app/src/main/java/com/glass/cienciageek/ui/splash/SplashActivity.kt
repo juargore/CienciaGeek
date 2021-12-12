20ed08f9dc734ef7a378edbc111dd11b5904d292
Arturo Gomez <arturog@bluetrailsoft.com>
2021-12-12 16:45:41 -0600

    feat: Splash screen added

---
+++ app/src/main/java/com/glass/cienciageek/ui/splash/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.glass.cienciageek.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.glass.cienciageek.R
+import com.glass.cienciageek.ui.MainActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+@Suppress("DEPRECATION")
+@SuppressLint("CustomSplashScreen", "SetTextI18n")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashScreenTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setCurrentVersionCode()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2500)
+    }
+
+    private fun setCurrentVersionCode() {
+        packageManager?.getPackageInfo(
+            packageName,
+            PackageManager.GET_ACTIVITIES)?.apply {
+            txtVersion.text = "Version $versionName"
+        }
+    }
+}
